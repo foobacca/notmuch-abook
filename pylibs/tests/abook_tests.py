80c3e58d32fe0db9a7ec213fef6536735c8fc619
Hamish Downer <hamish@aptivate.org>
2013-09-20 11:56:39 +0100

    start adding tests for SQLiteStorage

--- pylibs/tests/abook_tests.py
+++ pylibs/tests/abook_tests.py
@@ -5,6 +5,11 @@ import unittest
 
 abook_dir = path.join(path.dirname(__file__), os.pardir)
 testdata_dir = path.join(abook_dir, os.pardir, 'testdata')
+test_config = path.join(testdata_dir, 'notmuch-config')
+test_config_noignore = path.join(testdata_dir, 'notmuch-config-noignore')
+
+testdb = 'testdb.sqlite'
+testignore = 'testignore'
 
 sys.path.append(abook_dir)
 import notmuch_abook as abook
@@ -12,21 +17,45 @@ import notmuch_abook as abook
 
 class TestNotmuchConfig(unittest.TestCase):
 
+    # these two tests also confirm that our global variables have the correct
+    # values in
     def test_config_gets_backend(self):
-        test_config = path.join(testdata_dir, 'notmuch-config')
         config = abook.NotMuchConfig(test_config)
         self.assertEqual('sqlite', config.get('addressbook', 'backend'))
+        self.assertEqual(testdb, config.get('addressbook', 'path'))
 
     def test_config_gets_ignorefile_if_present(self):
-        test_config = path.join(testdata_dir, 'notmuch-config')
         config = abook.NotMuchConfig(test_config)
-        self.assertEqual('testignore', config.get('addressbook', 'ignorefile'))
+        self.assertEqual(testignore, config.get('addressbook', 'ignorefile'))
 
     def test_config_sets_ignorefile_to_None_if_not_present(self):
-        test_config = path.join(testdata_dir, 'notmuch-config-noignore')
-        config = abook.NotMuchConfig(test_config)
+        config = abook.NotMuchConfig(test_config_noignore)
         self.assertIsNone(config.get('addressbook', 'ignorefile'))
 
 
 class TestAbookDatabase(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.config = abook.NotMuchConfig(test_config)
+        self.db = abook.SQLiteStorage(self.config)
+
+    def tearDown(self):
+        if path.exists(testdb):
+            os.remove(testdb)
+
+    def createFakeDb(self):
+        with open(testdb, 'w') as f:
+            f.write('nonsense')
+
+    def test_path_set_correctly(self):
+        self.assertEqual(testdb, self.db._SQLiteStorage__path)
+
+    def test_connect_raises_IOError_when_db_not_present(self):
+        # this is checking our assumption
+        self.assertFalse(path.exists(testdb))
+        # this is checking the code
+        self.assertRaises(IOError, self.db.connect)
+
+    def test_create_raises_IOError_when_db_is_present(self):
+        self.createFakeDb()
+        self.assertRaises(IOError, self.db.create)
